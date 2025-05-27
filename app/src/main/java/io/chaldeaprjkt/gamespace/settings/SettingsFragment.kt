27e3122b12729b83cab759bb9cd51b2a8241ee9e
ImSpiDy <SpiDy2713@gmail.com>
2025-05-27 13:54:10 +0000

    Revert "Gamespace: Add support for OEM bypass charging"
    This reverts commit aa325f7ee061bc0e9b97568f1feac736144f93d4.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- app/src/main/java/io/chaldeaprjkt/gamespace/settings/SettingsFragment.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/settings/SettingsFragment.kt
@@ -20,12 +20,10 @@ package io.chaldeaprjkt.gamespace.settings
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.os.SystemProperties
 import android.provider.Settings
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.preference.Preference
-import androidx.preference.PreferenceCategory
 import androidx.preference.PreferenceFragmentCompat
 import dagger.hilt.android.AndroidEntryPoint
 import io.chaldeaprjkt.gamespace.R
@@ -59,16 +57,11 @@ class SettingsFragment : Hilt_SettingsFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val isGoogleDevice = Build.MANUFACTURER.equals("Google", ignoreCase = true)
-        val bypassSupported = SystemProperties.getBoolean("persist.sys.battery_bypass_supported", false)
         val bypassChargePref = findPreference<Preference>("bypass_charge_enabled")
-
-        if (!isGoogleDevice && !bypassSupported && bypassChargePref != null) {
-            val category = findPreference<Preference>("in_game_preferences") as? PreferenceCategory
-            category?.removePreference(bypassChargePref)
+        if (!isGoogleDevice && bypassChargePref != null) {
+            preferenceScreen.removePreference(bypassChargePref)
         }
-
         apps = findPreference(Settings.System.GAMESPACE_GAME_LIST)
         apps?.onRegisteredAppClick {
             perAppResult.launch(Intent(context, PerAppSettingsActivity::class.java).apply {
